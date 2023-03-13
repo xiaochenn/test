bfdf5e26540d3869e2ae1d9f16bb914745ca132f
xiaochenn <1173878818@qq.com>
2023-03-13 19:25:35 +0800

    3

--- lesson2.py
+++ lesson2.py
@@ -1,15 +1,30 @@
 
 
-a = list()
-for i in range(1,100):
-    flag = 0
-    for j in range(2,i):
-        if (i % j == 0):
-            flag = 1
-    if (flag == 0):
-        a.append(i)
-print(a)
+# a = list()
+# for i in range(1,100):
+#     flag = 0
+#     for j in range(2,i):
+#         if (i % j == 0):
+#             flag = 1
+#     if (flag == 0):
+#         a.append(i)
+# print(a)
+
+# j = sum(X[0] + X[i] + 3 * X[i+1] for i in range(10) if i % 2 == 1)
+# for y in range(10):
+#     if ((y + j) % 10 == 0):
+#         X.append(y)
+
+# code_table = {"S":"...", "O":"---"}
+# S = "SOS"
+# t = list(S)
+# p = " "
+# for i in range(t):
+#     p = p + code_table[t] + " "
+# print(p)
             
-        
+a = list()
+b = "sos"
+print(b)
 
     
