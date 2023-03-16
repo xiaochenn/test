a5d3e7272279818df0d1fa817cd0ae9d7bcd6d9c
xiaochenn <1173878818@qq.com>
2023-03-16 23:49:15 +0800

    finish

--- turtle and math.py
+++ turtle and math.py
@@ -1,4 +1,5 @@
 from random import *
+import turtle
 #coding = UTF-8
 
 
@@ -11,7 +12,64 @@ def sub(n):             #减法题的辅助函数，生成减数
 def mul(n):             #乘法题的辅助函数，生成乘数
     return choice(range(100//n+1))
 
+def write_square():    #画方框
+    turtle.setheading(0)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+
+def move_turtle(turtle, x, y):      #无痕移动
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+
+def write_question(turtle, question, x, y):   #写问题部分
+    move_turtle(turtle, x, y)
+    turtle.write(question, font=("Arial", 16, "normal"))
+    move_turtle(turtle, x+100, y)
+    write_square()
+
+def write_answer(turtle, answer, x, y):    #写答案部分
+    move_turtle(turtle, x, y)
+    turtle.write(answer, font=("Arial", 16, "normal"))
+
+def write_title(turtle, title, x, y):      #写标题部分
+    move_turtle(turtle, x, y)
+    turtle.write(title, font=("Arial", 16, "normal"))
+    move_turtle(turtle,x + 10, y)
+    turtle.setheading(0)
+    turtle.forward(600)                     #画横线
+
+def create_paper(timu,num_questions_per_row=3, num_rows=5, paper_width=0.99, paper_height=0.99):
+    turtle.setup(paper_width, paper_height)               #准备部分
+    turtle.speed(0)
+    turtle.hideturtle()
+    turtle.penup()
     
+    write_title(turtle, "Quiz", -600, 300)                 #问题部分
+    for i in range(num_rows):
+        for j in range(num_questions_per_row):
+            question = ' '.join(timu[(3 * i + j)][0:4])
+            x = -550 + j * 200                             #控制题目的间隔
+            y = 260 + i * -50
+            write_question(turtle, question, x, y)
+
+    write_title(turtle, "Answer", -600, 30)                 #答案部分
+    for i in range(num_rows):
+        for j in range(num_questions_per_row):
+            question = ' '.join(timu[(3 * i + j)][0:4])
+            answer = str(timu[(3 * i + j)][4])
+            x = -550 + j * 200
+            y = -10 + i * -50
+            write_question(turtle, question, x, y)
+            write_answer(turtle, answer, x + 105, y)
+    
+    turtle.exitonclick()
 
 if __name__ == '__main__':
     try:
@@ -29,40 +87,41 @@ if __name__ == '__main__':
         
         n = sample(range(51),3)      #将加法加入题目列表
         for i in n:
-            tem = [i,'+']
+            tem = [str(i),'+']
             t = add(i)
-            tem.append(t)
+            tem.append(str(t))          #将数字转化为字符串，方便之后海龟画图的整合
             tem.append('=')
-            tem.append(i+t)
+            tem.append(str(i+t))
             timu.append(tem)
 
         n = sample(divisition,4)    #将除法加入题目列表
         for i in n:
-            tem = [i[0],'/']
+            tem = [str(i[0]),'/']
             t = choice(i[1:-1])     #除数取除自己和1的数，否则太简单了
-            tem.append(t)
+            tem.append(str(t))
             tem.append('=')
-            tem.append(i[0]//t)
+            tem.append(str(i[0]//t))
             timu.append(tem)
         
         n = sample(range(1,100),4)  #将减法加入题目列表
         for i in n:
-            tem = [i,'-']
+            tem = [str(i),'-']
             t = sub(i)
-            tem.append(t)
+            tem.append(str(t))
             tem.append('=')
-            tem.append(i-t)
+            tem.append(str(i-t))
             timu.append(tem)
 
         n = sample(range(50),4)   #将乘法加入题目列表
         for i in n:
-            tem = [i,'x']
+            tem = [str(i),'x']
             t = mul(i)
-            tem.append(t)
+            tem.append(str(t))
             tem.append('=')
-            tem.append(i * t)
+            tem.append(str(i * t))
             timu.append(tem)
 
-        
+        shuffle(timu)          #题目乱序
+        create_paper(timu)      #生成卷子
     except:
         print("something wrong")
