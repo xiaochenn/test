20be5640074020da4ca66676041ccc203c5d82a1
xiaochenn <1173878818@qq.com>
2023-03-16 21:14:56 +0800

    homework

---
+++ turtle and math.py
@@ -0,0 +1,25 @@
+from random import *
+#coding = UTF-8
+
+
+def add(n):             #加法题的辅助函数,生成被加数
+    return choice(range(0,100,1))
+
+def sub(n):             #减法题的辅助函数，生成减数
+    return choice(range(0,n+1,1))
+
+def mul(n):             #乘法题的辅助函数，生成乘数
+    return choice(range(0,100//n+1,1))
+
+def ifPrime(n):
+   pass 
+    
+
+if __name__ == '__main__':
+    try:
+        timu = []
+        divisition = []
+        timu.append(1)
+        print(timu)
+    except:
+        pass
